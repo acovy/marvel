64bb9363fe0c3d204f96feeff820447a69d33809
Andrey <aseminmma@gmail.com>
2023-10-22 13:38:08 +0300

    добавили клики по комиксами и информацию о них с возвращением на предыдущую страницу

--- marvel/src/components/app/App.js
+++ marvel/src/components/app/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import { MainPage, ComicsPage } from "../pages";
+import { MainPage, ComicsPage, Page404, SingleComicPage} from "../pages";
 import AppHeader from "../appHeader/AppHeader";
 // все что внутри него до метода рендер умещает в себе логику и вычисления 
 const App = () => {
@@ -15,6 +15,8 @@ const App = () => {
                         <Routes>
                             <Route path="/" element={<MainPage/>}/>
                             <Route path="/comics" element={<ComicsPage/>}/>
+                            <Route path="/comics/:comicId" element={<SingleComicPage/>}/>
+                            <Route path="*" element={<Page404/>}/>
                         </Routes>
                     </main>
                 </div>

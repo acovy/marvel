4d119065e96a8eee66226729e08b0364fd58bafa
Andrey <aseminmma@gmail.com>
2023-10-22 16:10:11 +0300

    add lazy

--- marvel/src/components/app/App.js
+++ marvel/src/components/app/App.js
@@ -1,7 +1,13 @@
+import {lazy, Susp, Suspense} from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import { MainPage, ComicsPage, Page404, SingleComicPage} from "../pages";
 import AppHeader from "../appHeader/AppHeader";
+import Spinner from "../spinner/Spinner";
+
+const Page404 = lazy(() => import("../pages/404"));
+const MainPage = lazy(() => import("../pages/MainPage"));
+const SingleComicPage = lazy(() => import("../pages/SingleComicPage"));
+const ComicsPage = lazy(() => import("../pages/ComicsPage"));
 // все что внутри него до метода рендер умещает в себе логику и вычисления 
 const App = () => {
         // тут джсикс который возвращает разметку с логикой
@@ -12,12 +18,14 @@ const App = () => {
                 {/* шапка страницы */}
                     <AppHeader/>
                     <main>
-                        <Routes>
-                            <Route path="/" element={<MainPage/>}/>
-                            <Route path="/comics" element={<ComicsPage/>}/>
-                            <Route path="/comics/:comicId" element={<SingleComicPage/>}/>
-                            <Route path="*" element={<Page404/>}/>
-                        </Routes>
+                        <Suspense fallback={<Spinner/>}>
+                            <Routes>
+                                <Route path="/" element={<MainPage/>}/>
+                                <Route path="/comics" element={<ComicsPage/>}/>
+                                <Route path="/comics/:comicId" element={<SingleComicPage/>}/>
+                                <Route path="*" element={<Page404/>}/>
+                            </Routes>
+                        </Suspense>
                     </main>
                 </div>
             </Router>
